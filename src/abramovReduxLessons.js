1a432efbeb69a2f4f0ccb244d9fd9010f486158c
Rustam Apaev <rapaev@abisoft.biz>
2017-11-13 19:54:19 +0300

    some ESLint refactoring

--- src/abramovReduxLessons.js
+++ src/abramovReduxLessons.js
@@ -1,107 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import Counter from './Counter';
 
-//Reducer
+// Reducer
 export const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
-const store = createStore(counter)
-
-const Counter = ({
-    value, 
-    onIncrement, 
-    onDecrement
-    }) => (
-    <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-    </div>
-)
+const store = createStore(
+  counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={ store.getState() }
-            onIncrement={ ()=> store.dispatch({type:'INCREMENT'}) }
-            onDecrement={ ()=> store.dispatch({type:'DECREMENT'}) }
-        />, 
-        document.querySelectorAll('.counter')[0]
-    )
-}
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    document.querySelectorAll('.counter')[0],
+  );
+};
 
 store.subscribe(render);
-setTimeout( () => render(), 1000);
+setTimeout(() => render(), 1000);
 
-export const addCounter = (list) => {
-    //return list.concat([0]);
-    return [...list, 0]
-}
-
-export const removeCounter = (list, index) => {
-    return list.filter( (e, i) => {
-        return i !== index
-    })
-/* 
-    // alternatives : 
+export const addCounter = list =>
+  // return list.concat([0]);
+  [...list, 0];
+export const removeCounter = (list, index) => list.filter((e, i) => i !== index);
+/*
+    // alternatives :
 
     return list
         .slice(0, index)
         .concat(list.slice(index + 1))
     return [
-        ...list.slice(0, index), 
+        ...list.slice(0, index),
         ...list.slice(index + 1)
     ];
      */
-}
 
-export const incrementCounter = (list, index) => {
-    return list.map( (elem, i) => {
-        if ( i==index ) elem ++ 
-        return elem
-    })
-/* 
-    //alternative: 
+export const incrementCounter = (list, index) =>
+  list.map((elem, i) => {
+    if (i === index) elem += 1;
+    return elem;
+  });
+/*
+    //alternative:
     return [
-        ...list.slice(0, index), 
-        list[index] + 1, 
+        ...list.slice(0, index),
+        list[index] + 1,
         ...list.slice(index + 1)
     ]
      */
-}
 
-export const toggleTodo = (todo) => {
-    //ES7 - object spread 
-    return {
-        ...todo, 
-        completed: !todo.completed
-    }
-/*  
-    //ES6 alternative 
-    return Object.assign({}, todo, {
-        completed: !todo.completed
-    }) 
-*/
-/* 
-    //mutable version: 
-    todo.completed = !todo.completed
-    return todo;
-*/
-/*
-    //bad alternative: 
-    return {
-        id: todo.id, 
-        text: todo.text, 
-        completed: !todo.completed
-    }
- */
-} 
+export const toggleTodo = todo =>
+  // ES7 - object spread
+  ({
+    ...todo,
+    completed: !todo.completed,
+  });
--- src/abramovReduxLessons.spec.js
+++ src/abramovReduxLessons.spec.js
@@ -1,54 +1,58 @@
-import { counter, addCounter, removeCounter, 
-    incrementCounter, toggleTodo } from './abramovReduxLessons'
-import deepFreeze  from 'deep-freeze'
+import deepFreeze from 'deep-freeze';
+import {
+  counter,
+  addCounter,
+  removeCounter,
+  incrementCounter,
+  toggleTodo,
+} from './abramovReduxLessons';
+
 test('Counter reducer ', () => {
-    expect(counter(0, { type: 'INCREMENT' })).toBe(1)
-    expect(counter(1, { type: 'INCREMENT' })).toBe(2)
-    expect(counter(2, { type: 'DECREMENT' })).toBe(1)
-    expect(counter(0, { type: 'DECREMENT' })).toBe(-1)
-    expect(counter(undefined, {})).toBe(0)
+  expect(counter(0, { type: 'INCREMENT' })).toBe(1);
+  expect(counter(1, { type: 'INCREMENT' })).toBe(2);
+  expect(counter(2, { type: 'DECREMENT' })).toBe(1);
+  expect(counter(0, { type: 'DECREMENT' })).toBe(-1);
+  expect(counter(undefined, {})).toBe(0);
 });
 
 test('addCounter immutable ', () => {
+  const listBefore = [];
+  const listAfter = [0];
 
-    const listBefore = []
-    const listAfter = [0]
+  deepFreeze(listBefore);
 
-    deepFreeze(listBefore)
-
-    expect( addCounter(listBefore)).toEqual(listAfter) 
-
+  expect(addCounter(listBefore)).toEqual(listAfter);
 });
 
 test('removeCounter immutable', () => {
-    const listBefore = [0, 10, 20, 30, 40]
-    const listAfter = [0, 10, 20, 40]
-    deepFreeze(listBefore)
+  const listBefore = [0, 10, 20, 30, 40];
+  const listAfter = [0, 10, 20, 40];
+  deepFreeze(listBefore);
 
-    expect( removeCounter(listBefore, 3)).toEqual(listAfter) 
-})
+  expect(removeCounter(listBefore, 3)).toEqual(listAfter);
+});
 
 test('testIncrementCounter immutable', () => {
-    const listBefore = [0, 10, 20]
-    const listAfter = [0, 11, 20]
-    deepFreeze(listBefore)
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+  deepFreeze(listBefore);
 
-    expect( incrementCounter(listBefore, 1)).toEqual(listAfter) 
-})
+  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+});
 
 test('testToggleTodo immutable', () => {
-    const todoBefore = {
-        id: 0, 
-        text: 'Learn Redux', 
-        completed: false
-    }
-    const todoAfter = {
-        id: 0, 
-        text: 'Learn Redux', 
-        completed: true
-    }
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false,
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true,
+  };
 
-    deepFreeze(todoBefore)
+  deepFreeze(todoBefore);
 
-    expect( toggleTodo(todoBefore)).toEqual(todoAfter) 
-})
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
+});
