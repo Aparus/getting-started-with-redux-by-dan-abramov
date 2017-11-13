90a0a4a1a3551ecb1bd56f4e34c3271d4e21e876
Rustam Apaev <rapaev@abisoft.biz>
2017-11-13 15:58:29 +0300

    9. Redux: Avoiding Array Mutations with concat(), slice(), and ...spread - COMPLETED

--- src/abramovReduxLessons.js
+++ src/abramovReduxLessons.js
@@ -41,3 +41,40 @@ const render = () => {
 
 store.subscribe(render);
 setTimeout( () => render(), 1000);
+
+export const addCounter = (list) => {
+    //return list.concat([0]);
+    return [...list, 0]
+}
+
+export const removeCounter = (list, index) => {
+    return list.filter( (e, i) => {
+        return i !== index
+    })
+/* 
+    // alternatives : 
+
+    return list
+        .slice(0, index)
+        .concat(list.slice(index + 1))
+    return [
+        ...list.slice(0, index), 
+        ...list.slice(index + 1)
+    ];
+     */
+}
+
+export const incrementCounter = (list, index) => {
+    return list.map( (elem, i) => {
+        if ( i==index ) elem ++ 
+        return elem
+    })
+/* 
+    //alternative: 
+    return [
+        ...list.slice(0, index), 
+        list[index] + 1, 
+        ...list.slice(index + 1)
+    ]
+     */
+}
--- src/abramovReduxLessons.spec.js
+++ src/abramovReduxLessons.spec.js
@@ -1,10 +1,36 @@
-import { counter } from './abramovReduxLessons'
-
+import { counter, addCounter, removeCounter, incrementCounter } from './abramovReduxLessons'
+import deepFreeze  from 'deep-freeze'
 test('Counter reducer ', () => {
     expect(counter(0, { type: 'INCREMENT' })).toBe(1)
     expect(counter(1, { type: 'INCREMENT' })).toBe(2)
     expect(counter(2, { type: 'DECREMENT' })).toBe(1)
     expect(counter(0, { type: 'DECREMENT' })).toBe(-1)
     expect(counter(undefined, {})).toBe(0)
+});
 
+test('addCounter immutable ', () => {
+
+    const listBefore = []
+    const listAfter = [0]
+
+    deepFreeze(listBefore)
+
+    expect( addCounter(listBefore)).toEqual(listAfter) 
+
+});
+
+test('removeCounter immutable', () => {
+    const listBefore = [0, 10, 20, 30, 40]
+    const listAfter = [0, 10, 20, 40]
+    deepFreeze(listBefore)
+
+    expect( removeCounter(listBefore, 3)).toEqual(listAfter) 
+})
+
+test('testIncrementCounter immutable', () => {
+    const listBefore = [0, 10, 20]
+    const listAfter = [0, 11, 20]
+    deepFreeze(listBefore)
+
+    expect( incrementCounter(listBefore, 1)).toEqual(listAfter) 
 })
