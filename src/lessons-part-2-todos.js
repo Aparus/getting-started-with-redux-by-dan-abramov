8084f82a6d9e517065840e4db1f8258726d9196a
Rustam Apaev <rapaev@abisoft.biz>
2017-11-14 19:02:29 +0300

    lesson 17. Redux: React Todo List Example (Adding a Todo) - COMPLETED

--- src/lessons-part-2-todos.js
+++ src/lessons-part-2-todos.js
@@ -1,5 +1,5 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom'; */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 
 export const toggleTodo = todo =>
@@ -63,3 +63,40 @@ const store = createStore(
   todoApp,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          ref={(node) => {
+            this.input = node;
+          }}
+        />
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: Date.now(),
+            });
+            this.input.value = '';
+          }}
+        >
+          Add Todo
+        </button>
+        <ul>{this.props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
+      </div>
+    );
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.querySelectorAll('.todos')[0],
+  );
+};
+
+store.subscribe(render);
+setTimeout(() => render(), 1000);
